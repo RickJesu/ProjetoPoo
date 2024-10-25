71542a4ae9b2d564e6edc352d5c4c1bcd0e9995c
Henrique Jesus <161508735+RickJesu@users.noreply.github.com>
2024-10-25 11:21:10 -0300

    Add files via upload

---
+++ ProjetoPoo/ProjetoAulaPOO/src/main/java/br/senac/sp/projetopoo/modelo/Marca.java
@@ -0,0 +1,26 @@
+package br.senac.sp.projetopoo.modelo;
+
+public class Marca {
+	private int id;
+	private String nome;
+	private byte [] logo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public byte[] getLogo() {
+		return logo;
+	}
+	public void setLogo(byte[] logo) {
+		this.logo = logo;
+	}
+}
